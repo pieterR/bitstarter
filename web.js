d27ef4fe47852f5aaeca6e91edb3e4195f92d6b5
Pieter Reyneke <pieter.reyneke@gmail.com>
2013-07-09 20:51:41 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // response.send('Hello World 2!');
+ //fs.readFileSync('index.html', [options]);
+ buf = new Buffer(256);
+ buf = fs.readFileSync('foo.tx', 'utf8');
+ var text = buf.toString('utf8');
+ response.send(text);
 });
 
 var port = process.env.PORT || 5000;
